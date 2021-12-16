c5e558dfefdb4550add99f2e607a02dc5e401290
hpf3 <redconman007@gmail.com>
2021-12-15 17:08:59 -0800

    added solution file, and day1-1

---
+++ day1/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] input = new int[] {
+199,
+200,
+208,
+210,
+200,
+207,
+240,
+269,
+260,
+263
+};
+int result = 0;
+for (int i = 1; i < input.Length; i++)
+			{
+	if (input[i] > input[i-1])
+	{
+		result++;
+	}
+			}
+Console.WriteLine(result);
+Console.ReadLine();
