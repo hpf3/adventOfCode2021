9a4d5950e5a280e2c933ea15c402af3af1b80ba6
hpf3 <redconman007@gmail.com>
2021-12-15 18:06:21 -0800

    day1-2 complete

---
+++ day1-2/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] input = day1_2.input.Input;
+int result = 0;
+System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+sw.Start();
+int A = input[0]+input[1]+input[2];
+for (int i = 3; i < input.Length; i++)
+{
+	int B = input[i-2]+input[i-1]+input[i];
+	if (B > A)
+	{
+		result++;
+	}
+	A = B;
+}
+sw.Stop();
+Console.WriteLine($"result: {result} time: {sw.Elapsed}");
+Console.ReadLine();
