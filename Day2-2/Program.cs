7da27fd27d026f044bf102b5ce5ed9d3effd1ea6
hpf3 <18592150+hpf3@users.noreply.github.com>
2021-12-16 15:26:20 -0800

    Day2-2 Complete

---
+++ Day2-2/Program.cs
@@ -0,0 +1,28 @@
+﻿List<KeyValuePair<string, int>> input = day2.Input.Data;
+int X = 0;
+int Y = 0;
+int Aim = 0;
+System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+sw.Start();
+foreach (KeyValuePair<string, int> item in input)
+{
+    switch (item.Key)
+    {
+        case "forward":
+            X += item.Value;
+            Y += Aim*item.Value;
+            break;
+        case "up":
+            Aim -= item.Value;
+            break;
+        case "down":
+            Aim += item.Value;
+            break;
+        default:
+            break;
+    }
+}
+int Result = X * Y;
+sw.Stop();
+Console.WriteLine($"result: {Result} Time:{sw.Elapsed}");
+Console.ReadLine();
