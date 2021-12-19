d9e1047f394a23c8dcaeb3176d72ee0d329ad950
hpf3 <18592150+hpf3@users.noreply.github.com>
2021-12-18 16:44:07 -0800

    Day7-2 Complete

---
+++ Day7-2/Program.cs
@@ -0,0 +1,38 @@
+﻿string[] Input = Day7.ChallengeInput.Data;
+System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+sw.Start();
+int[] crabs = new int[Input.Length];
+
+int Max = 0;
+int Min = 0;
+//convert the strings and set min&max
+for (int i = 0; i < Input.Length; i++)
+{
+    crabs[i] = int.Parse(Input[i]);
+    if (crabs[i] > Max) Max = crabs[i];
+    if (crabs[i] < Min) Min = crabs[i];
+}
+
+int result = int.MaxValue;
+int pos = 0;
+for (int a = Min; a <= Max; a++)
+{
+    int score = 0;
+
+    for (int b = 0; b < crabs.Length; b++)
+    {
+        int d = Math.Max(a, crabs[b]) - Math.Min(a, crabs[b]);
+        double tmp = (((d - 1d) / 2d + 1d) * d);//by the power of math smart siblings
+        score += (int)tmp;
+    }
+    if (score < result)
+    {
+        pos = a;
+        result = score;
+    }
+}
+sw.Stop();
+Console.WriteLine($"Pos:{pos}");
+Console.WriteLine($"Result: {result} Time: {sw.Elapsed}");
+Console.ReadLine();
